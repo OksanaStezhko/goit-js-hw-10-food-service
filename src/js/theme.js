7e4ac00e6b572f283e34e4b0ad35d863b18a883a
Oksana Stezhko <oksana_stegko@ukr.net>
2021-01-13 13:07:27 +0200

    рабочий вариант

--- src/js/theme.js
+++ src/js/theme.js
@@ -2,29 +2,31 @@ const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
 const checkboxRef = document.querySelector('#theme-switch-toggle');
 const bodyRef = document.body;
 
-
-
-function getSavedTheme(){
+function setSavedTheme() {
   const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === Theme.DARK) {
-    checkboxRef.checked = true;
-
-    bodyRef.classList.add(savedTheme);} else {bodyRef.classList.add(Theme.LIGHT);};
-};
-function onChangeTherm(){
-
-    if ( bodyRef.classList.contains(Theme.LIGHT)){
-      bodyRef.classList.replace(Theme.LIGHT, Theme.DARK);
-      localStorage.setItem('theme', Theme.DARK);
-    }
-    else { bodyRef.classList.replace(Theme.DARK, Theme.LIGHT);
-       localStorage.setItem('theme', Theme.LIGHT );
-    };
-
-
+  bodyRef.classList.add(savedTheme);
+  checkboxRef.checked = savedTheme === Theme.DARK;
 }
-getSavedTheme();
+
+function setChangedTheme(oldTheme, newTheme) {
+  bodyRef.classList.replace(oldTheme, newTheme);
+  localStorage.setItem('theme', newTheme);
+}
+
+function checkClass(checkedClass) {
+  return bodyRef.classList.contains(checkedClass);
+}
+
+function onChangeTherm() {
+  if (checkClass(Theme.LIGHT)) {
+    setChangedTheme(Theme.LIGHT, Theme.DARK);
+  } else {
+    setChangedTheme(Theme.DARK, Theme.LIGHT);
+  }
+}
+setSavedTheme();
 checkboxRef.addEventListener('change', onChangeTherm);
